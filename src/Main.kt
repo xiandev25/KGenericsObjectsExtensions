99e791763cc9884188e780639a8dd9b8c2533664
Xian Dev <xiandev25@gmail.com>
2025-11-25 18:14:25 +0100

    Call the toString() method on a data class instance

--- src/Main.kt
+++ src/Main.kt
@@ -30,4 +30,5 @@ fun main() {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    println(question1.toString())
 }
