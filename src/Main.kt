0afc5da9619cf6d73813791c27bb6c93eac2b8d7
Xian Dev <xiandev25@gmail.com>
2025-11-25 18:00:38 +0100

    Refactor the code to use generics

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,24 @@
-class FillInTheBlankQuestion(
-    val questionText: String,
-    val answer: String,
-    val difficulty: String
-)
+//class FillInTheBlankQuestion(
+//    val questionText: String,
+//    val answer: String,
+//    val difficulty: String
+//)
+//
+//class TrueOrFalseQuestion(
+//    val questionText: String,
+//    val answer: Boolean,
+//    val difficulty: String
+//)
+//
+//class NumericQuestion(
+//    val questionText: String,
+//    val answer: Int,
+//    val difficulty: String
+//)
 
-class TrueOrFalseQuestion(
+class Question<T>(
     val questionText: String,
-    val answer: Boolean,
-    val difficulty: String
-)
-
-class NumericQuestion(
-    val questionText: String,
-    val answer: Int,
+    val answer: T,
     val difficulty: String
 )
 
