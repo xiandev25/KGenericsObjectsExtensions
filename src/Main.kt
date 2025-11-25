4fb2bea4db2532c2362e6c4fc771024fe578e119
Xian Dev <xiandev25@gmail.com>
2025-11-25 17:58:16 +0100

    Define 3 classes that differ with one property type

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class FillInTheBlankQuestion(
+    val questionText: String,
+    val answer: String,
+    val difficulty: String
+)
+
+class TrueOrFalseQuestion(
+    val questionText: String,
+    val answer: Boolean,
+    val difficulty: String
+)
+
+class NumericQuestion(
+    val questionText: String,
+    val answer: Int,
+    val difficulty: String
+)
+
 fun main() {
 }
