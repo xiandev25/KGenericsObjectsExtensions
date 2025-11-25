ae80cfa24cbe215d49d6297c865caa67212ec8fd
Xian Dev <xiandev25@gmail.com>
2025-11-25 18:09:51 +0100

    Use the Difficulty enum class as the difficulty property type

--- src/Main.kt
+++ src/Main.kt
@@ -19,7 +19,7 @@
 class Question<T>(
     val questionText: String,
     val answer: T,
-    val difficulty: String
+    val difficulty: Difficulty
 )
 
 enum class Difficulty {
@@ -27,7 +27,7 @@ enum class Difficulty {
 }
 
 fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
-    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
-    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 }
