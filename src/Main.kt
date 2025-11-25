9d45dba0a18f65e45cced35867937bae4cbb8a9f
Xian Dev <xiandev25@gmail.com>
2025-11-25 18:04:50 +0100

    Instantiate 3 Question objects with different types of answers

--- src/Main.kt
+++ src/Main.kt
@@ -23,4 +23,7 @@ class Question<T>(
 )
 
 fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
 }
