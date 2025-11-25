6279825956e1424fc7e777fbb7464cc2e4b25aa2
Xian Dev <xiandev25@gmail.com>
2025-11-25 18:59:30 +0100

    Create and use an extension method for the companion object

--- src/Main.kt
+++ src/Main.kt
@@ -40,6 +40,13 @@ class Quiz {
 val Quiz.StudentProgress.progressText: String
     get() = "${Quiz.answered} of ${Quiz.total} answered."
 
-fun main() {
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓")}
+    repeat(Quiz.total - Quiz.answered) { print("▒")}
+    println()
     println(Quiz.progressText)
+}
+
+fun main() {
+    Quiz.printProgressBar()
 }
