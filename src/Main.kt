b7389394d660565e5b3a5752d9a6ee6b337f373c
Xian Dev <xiandev25@gmail.com>
2025-11-25 18:28:30 +0100

    Create and use a companion object inside the new Quiz class

--- src/Main.kt
+++ src/Main.kt
@@ -26,15 +26,17 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-object StudentProgress {
-    var total: Int = 10
-    var answered: Int = 3
-}
-
-fun main() {
+class Quiz {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-    println(question1.toString())
-    println("${StudentProgress.answered} of ${StudentProgress.total} answered.")
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+fun main() {
+    println("${Quiz.answered} of ${Quiz.total} answered.")
 }
